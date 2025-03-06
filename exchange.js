b18f0547abbc71f325400dc6ef1f9ae8cd07b37f
chinh-2o04 <hoangchinh18hvc@gmail.com>
2025-03-06 17:22:58 +0700

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,8 +1,10 @@
 function convertXu(rate) {
     let input = document.getElementById("xuInput").value;
     let values = input.match(/\d+/g);
-    let total = values ? values.reduce((sum, val) => sum + parseInt(val), 0) : 0;
-    document.getElementById("result").textContent = `Số tiền nhận được: ${(total / 1000 * rate).toLocaleString()} VND`;
+    let totalXu = values ? values.reduce((sum, val) => sum + parseInt(val), 0) : 0;
+    let totalMoney = Math.floor((totalXu / 1000) * rate);
+    
+    document.getElementById("result").textContent = `Số tiền nhận được: ${totalMoney.toLocaleString()} VND`;
 }
 
 function customConvert() {
